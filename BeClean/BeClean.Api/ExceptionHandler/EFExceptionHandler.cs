0c861986b25391de40b49f5348433bf9a0a50a19
Hugo Paiement <paiementh@lichens.ai>
2026-03-23 16:31:27 -0400

    feat: Add LocalizedUnauthorizedException handling

--- BeClean/BeClean.Api/ExceptionHandler/EFExceptionHandler.cs
+++ BeClean/BeClean.Api/ExceptionHandler/EFExceptionHandler.cs
@@ -18,16 +18,45 @@ namespace BeClean.Api.ExceptionHandler
             _logger.LogError($"Exception occured during request handling: {exception}");
 
             await _unitOfWorkRegistry.RollbackAllAsync(); // Rollback any ongoing transactions. The Unit of work is passed by dependency injection
-            context.Response.ContentType = "text/plain";
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-            var message = exception.Message.ToString();
-            if (exception is LocalizedUserException lex)
-            {
-                message = string.Format(_localStringService.GetLocaleString(lex.Message), lex.Args);
-            }
+            SetResponseCode(context, exception);
+            string message = GetErrorMessage(exception);
             
             await context.Response.WriteAsync(message);
+        }
+
+        private void SetResponseCode(HttpContext context, Exception exception)
+        {
+            if (exception is UnauthorizedAccessException)
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+            }
+            else
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            }
+        }
+
+        private string GetErrorMessage(Exception exception)
+        {
+            if (exception is LocalizedUnauthorizedException luex)
+            {
+                return string.Format(_localStringService.GetLocaleString(luex.Message), luex.Args);
+            }
+            else if (exception is UnauthorizedAccessException)
+            {
+                return _localStringService.GetLocaleString("Unauthorized");
+            }
+            else if (exception is LocalizedUserException lex)
+            {
+                return string.Format(_localStringService.GetLocaleString(lex.Message), lex.Args);
+            }
+            else
+            {
+                return exception.Message.ToString();
+            }
         }
     }
 }
---
+++ BeClean/BeClean.Localization/LocalizedUnauthorizedException.cs
@@ -0,0 +1,12 @@
+﻿namespace BeClean.Localization
+{
+    public class LocalizedUnauthorizedException : UnauthorizedAccessException
+    {
+        public object[] Args { get; init; }
+
+        public LocalizedUnauthorizedException(string messageKey, params object[] args) : base(messageKey)
+        {
+            Args = args;
+        }
+    }
+}
