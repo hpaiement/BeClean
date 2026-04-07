65fd843bf200b5f0219870fdf6a5bcc7e49db883
Hugo Paiement <paiementh@lichens.ai>
2026-04-07 08:34:50 -0400

    feat(Util): Add NowGenerator class

---
+++ BeClean/BeClean.Util/NowGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace BeClean.Util
+{
+    /// <summary>
+    /// Returns the actual datetime
+    /// </summary>
+    public interface INowGenerator
+    {
+        DateTime UtcNow { get; }
+        DateTime Now { get; }
+    }
+
+    public class NowGenerator : INowGenerator
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+        public DateTime Now => DateTime.Now;
+    }
+}
